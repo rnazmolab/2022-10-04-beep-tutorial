55fc1b5c34e8543768cc8e31e97590730dc25b0e
rnazmo <rnazmo@gmail.com>
2022-10-04 06:29:32 +0900

    Add hello-beep.go

---
+++ hello-beep.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+func main() {
+	f, err := os.Open("Rockafeller Skank.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	speaker.Play(streamer)
+	select {}
+}
